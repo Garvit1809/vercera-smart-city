47b5553a8d35316f18a69bbbdf65147fef50ef32
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2023-02-21 11:58:54 +0530

    server set up

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
+dotenv.config({ path: "./.env" });
 
+const app = express();
 
-// app.listen = 
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_BASE_URL || "http://localhost:3000",
+  })
+);
+app.use(express.json({ limit: "10kb" }));
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => console.log("DB connection success"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running to port ${PORT}`);
+});
